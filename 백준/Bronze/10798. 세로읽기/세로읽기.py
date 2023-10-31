23d049e2e2d9817e0e0ac3ad811a67213b36fd91
D.H Github <dablro1232@gmail.com>
2023-10-31 19:16:55 +0900

    [Bronze I] Title: 세로읽기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,9 @@
+words = []
+for _ in range(5):
+    a = input()
+    words.append(a)
+
+for j in range(15):
+    for i in range(5):
+        if j < len(words[i]):
+            print(words[i][j], end='')
