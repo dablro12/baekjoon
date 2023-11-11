ed373f132c77f6273815c12b70a137522ed7dcef
D.H Github <dablro1232@gmail.com>
2023-11-11 12:17:02 +0900

    [Silver V] Title: 영화감독 숌, Time: 652 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,11 @@
+n = int(input())
+cnt = 0
+end_number = '666'
+result = 666
+while 1:
+    if '666' in str(result):
+        cnt += 1
+    if cnt == n:
+        print(result)
+        break
+    result+=1
