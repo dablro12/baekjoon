25b42fb70ef4365ab6f24e3de98fd23fb0b00b97
D.H Github <dablro1232@gmail.com>
2023-11-16 01:05:29 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 236 ms, Memory: 51260 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,21 +1,16 @@
 import sys
-
-n, m = map(int, input().split())
-
-# 리스트에 포켓몬 저장
-pokedic_int_key = {}  # Key값이 int인 dictionary
-pokedic_name_key = {}  # Key값이 str인 dictionary
-for i in range(n):
-    name = sys.stdin.readline().strip()
-    pokedic_int_key[i] = name
-    pokedic_name_key[name] = i
-
-# 포켓몬 탐색
+input = sys.stdin.readline
+n, m = map(int, input().strip().split()) #n,m 은 (1,100,000) 
+poketmon_dict = {}
+converted_poketmon_dict = {}
+for idx in range(n): #첫글자만 대문자, 나머지 문자는 소문자 *예외 : 일부 포켓몬은 마지막 문자만 대문자일수도 있다.
+    name = input().strip()
+    poketmon_dict[idx] = name
+    converted_poketmon_dict[name] = idx
+    
 for _ in range(m):
-    item = sys.stdin.readline().strip()
-    # 입력값이 int일 경우, key값이 int인 dictionary에서 value를 가져옴
-    if item.isdigit() == True:  # isdigit -> O(n)
-        print(pokedic_int_key[int(item)-1])
-    # 입력값이 int가 아닐 경우 (문자열일경우), key값이 str인 dictionary에서 value를 가져옴
-    else:
-        print(pokedic_name_key[item]+1)
+    quiz = input().strip()
+    if quiz.isdigit() == True:# quiz가 숫자로 들어오면 
+        print(poketmon_dict[int(quiz)-1]) #사전영어를 출력
+    else : #quiz가 영어로 들어오면
+        print(converted_poketmon_dict[quiz]+1) #숫자를 출력
